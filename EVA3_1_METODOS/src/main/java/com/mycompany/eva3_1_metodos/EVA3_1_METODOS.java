7e074612657de88035f75bede398f011bc53e89c
Jonathan Alexis Islas Estrada <L24550378@chihuahua2.tecnm.mx>
2024-11-22 16:37:51 -0600

    Add files via upload

---
+++ EVA3_1_METODOS/src/main/java/com/mycompany/eva3_1_metodos/EVA3_1_METODOS.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva3_1_metodos;
+
+/**
+ *
+ * @author ae478
+ */
+public class EVA3_1_METODOS {
+
+    public static void main(String[] args) {
+        imprimirMensaje("HOLA MUNDO!!");
+    }
+    public static void imprimirMensaje(String Mensaje){
+        System.out.println("Tu Mensaje: " + Mensaje);
+    }
+}
